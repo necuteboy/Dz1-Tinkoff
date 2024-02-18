37ed8206443732c3584db1db1b969d65099eb575
necuteboy <andreychikov32@mail.ru>
2024-02-18 19:00:12 +0500

    first commit

---
+++ src/test/java/com/example/dz1/Dz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
